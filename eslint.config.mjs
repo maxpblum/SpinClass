f8ef9a010cc0625ce113597e9291a9a7234fc4f3
Max Blum-Campo <max.blum@gmail.com>
2025-01-26 13:22:47 -0500

    Prettify

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,12 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 const reactSettings = pluginReact.configs.flat.recommended;
-reactSettings.settings = {react: {version: 'detect'}};
+reactSettings.settings = { react: { version: "detect" } };
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   reactSettings,
--- src/state_machine/state_machine.ts
+++ src/state_machine/state_machine.ts
@@ -1,7 +1,7 @@
-import {x} from './state_machine_dep.js';
+import { x } from "./state_machine_dep.js";
 
 function logWord(word: string) {
   console.log(word);
 }
 
-logWord('hello');
+logWord("hello");
