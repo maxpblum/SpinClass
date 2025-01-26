d8406236c1e3bfd6370d77857fe216a8d3f1f8b0
Max Blum-Campo <max.blum@gmail.com>
2025-01-26 13:00:40 -0500

    Set up example dependency relationship that builds properly

--- src/state_machine/state_machine.ts
+++ src/state_machine/state_machine.ts
@@ -1,2 +1,7 @@
-export class StateMachine<StateType, SideEffectType> {
+import {x} from './state_machine_dep.js';
+
+function logWord(word: string) {
+  console.log(word);
 }
+
+logWord(x as unknown as string);
