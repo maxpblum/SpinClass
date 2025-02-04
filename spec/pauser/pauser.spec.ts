f9c5975885ac80664240aafa677e7062756fec18
Max Blum-Campo <max.blum@gmail.com>
2025-02-04 07:37:51 -0500

    Get pauser into shape

---
+++ spec/pauser/pauser.spec.ts
@@ -0,0 +1,69 @@
+import {
+  makePauser,
+  makeTimeEvent,
+  PAUSE_EVENT,
+  RESUME_EVENT,
+} from '../../src/pauser/pauser.js';
+
+describe('makePauser', () => {
+  it('begins paused', () => {
+    const pauserFunc = makePauser();
+    expect(pauserFunc(makeTimeEvent(123)).paused).toBeTrue();
+  });
+
+  it('ignores elapsed time while first paused', () => {
+    const pauserFunc = makePauser();
+    // The first upstream time event doesn't count toward elapsed time.
+    pauserFunc(makeTimeEvent(100));
+    // Passing in a value of 200 after a value of 100 would increment .elapsed by 100 if not paused.
+    expect(pauserFunc(makeTimeEvent(200)).elapsed).toEqual(0);
+  });
+
+  it('respects resuming', () => {
+    const pauserFunc = makePauser();
+    expect(pauserFunc(RESUME_EVENT).paused).toBeFalse();
+  });
+
+  it('ignores elapsed time from before resuming even after resuming', () => {
+    const pauserFunc = makePauser();
+    // The first upstream time event doesn't count toward elapsed time.
+    pauserFunc(makeTimeEvent(100));
+    // Passing in a value of 200 after a value of 100 would increment .elapsed by 100 if not paused.
+    pauserFunc(makeTimeEvent(200));
+    expect(pauserFunc(RESUME_EVENT).elapsed).toEqual(0);
+  });
+
+  it('counts elapsed time after resuming', () => {
+    const pauserFunc = makePauser();
+    // The first upstream time event doesn't count toward elapsed time.
+    pauserFunc(makeTimeEvent(100));
+    pauserFunc(RESUME_EVENT);
+    // Passing in a value of 200 after a value of 100 should increment .elapsed by 100.
+    expect(pauserFunc(makeTimeEvent(200)).elapsed).toEqual(100);
+  });
+
+  it('counts multiple elapsed time increments after resuming', () => {
+    const pauserFunc = makePauser();
+    pauserFunc(makeTimeEvent(100));
+    pauserFunc(RESUME_EVENT);
+    pauserFunc(makeTimeEvent(200));
+    expect(pauserFunc(makeTimeEvent(300)).elapsed).toEqual(200);
+  });
+
+  it('respects pausing after resuming', () => {
+    const pauserFunc = makePauser();
+    pauserFunc(makeTimeEvent(100));
+    pauserFunc(RESUME_EVENT);
+    expect(pauserFunc(PAUSE_EVENT).paused).toBeTrue();
+  });
+
+  it('stops counting elapsed time after resume followed by pause', () => {
+    const pauserFunc = makePauser();
+    pauserFunc(makeTimeEvent(100));
+    pauserFunc(RESUME_EVENT);
+    pauserFunc(makeTimeEvent(200));
+    pauserFunc(PAUSE_EVENT);
+    // Pauser should have incremented at the first time event but not the second.
+    expect(pauserFunc(makeTimeEvent(300)).elapsed).toEqual(100);
+  });
+});
---
+++ src/pauser/pauser.ts
@@ -0,0 +1,67 @@
+import { makeStateMachine } from '../patcher/state_machine.js';
+
+interface PauserState {
+  readonly paused: boolean;
+  readonly elapsed: DOMHighResTimeStamp;
+  readonly lastUpstreamTime: DOMHighResTimeStamp;
+}
+
+interface PauserOutput {
+  readonly paused: boolean;
+  readonly elapsed: DOMHighResTimeStamp;
+}
+
+type Pause = {
+  readonly kind: 'pause';
+};
+
+export const PAUSE_EVENT: Pause = {kind: 'pause'};
+
+type Resume = {
+  readonly kind: 'resume';
+};
+
+export const RESUME_EVENT: Resume = {kind: 'resume'};
+
+type UpstreamTime = {
+  readonly kind: 'upstreamtime';
+  readonly time: DOMHighResTimeStamp;
+};
+
+export function makeTimeEvent(time: DOMHighResTimeStamp): UpstreamTime {
+  return {kind: 'upstreamtime', time};
+}
+
+type PauserEvent = Pause | Resume | UpstreamTime;
+
+const initialState: PauserState = {
+  paused: true,
+  elapsed: 0,
+  lastUpstreamTime: -1,
+};
+
+function reducer(state: PauserState, input: PauserEvent) {
+  const newState = {
+    lastUpstreamTime:
+      input.kind === 'upstreamtime' ? input.time : state.lastUpstreamTime,
+    paused: input.kind === 'pause' || (state.paused && input.kind !== 'resume'),
+    elapsed:
+      input.kind === 'upstreamtime' && !state.paused
+        ? state.elapsed + input.time - state.lastUpstreamTime
+        : state.elapsed,
+  };
+  return {
+    newState,
+    output: {
+      paused: newState.paused,
+      elapsed: newState.elapsed,
+    },
+  };
+}
+
+export function makePauser() {
+  return makeStateMachine<PauserState, PauserEvent, PauserOutput>(
+    initialState,
+    reducer
+  );
+}
