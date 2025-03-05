dbe5785a93ec298147e7ff9a374e0cc65303f93b
Max Blum-Campo <max.blum@gmail.com>
2025-03-05 12:46:37 -0600

    Unit test the scheduler

--- src/tempo/scheduler.ts
+++ src/tempo/scheduler.ts
@@ -1,6 +1,12 @@
-import {makeStateMachine} from '../blip.js';
-import {getHead, getTail, isEmptyList, makeList, LinkedList} from '../linked_list.js';
-import {NewTime, NewTempo, makeTempoEvent} from '../interfaces.js';
+import { makeStateMachine } from '../blip.js';
+import {
+  getHead,
+  getTail,
+  isEmptyList,
+  makeList,
+  LinkedList,
+} from '../linked_list.js';
+import { NewTime, NewTempo, makeTempoEvent } from '../interfaces.js';
 
 interface Change {
   tempo: number;
@@ -8,23 +14,37 @@ interface Change {
 }
 
 interface State {
+  // These should be in chronological order, earliest first (i.e. earliest at
+  // the "head" end of the list).
   changes: LinkedList<Change>;
 }
 
-const initialState: State = {changes: makeList([{tempo: 50, time: 0}, {tempo: 60, time: 1000}, {tempo: 70, time: 2000}, {tempo: 80, time: 3000}, {tempo: 90, time: 4000}, {tempo: 100, time: 5000}])};
+const initialState: State = {
+  changes: makeList([
+    { tempo: 50, time: 0 },
+    { tempo: 60, time: 1000 },
+    { tempo: 70, time: 2000 },
+    { tempo: 80, time: 3000 },
+    { tempo: 90, time: 4000 },
+    { tempo: 100, time: 5000 },
+  ]),
+};
 
-function reducer(s: State, input: NewTime): {newState: State, output?: NewTempo} {
+function reducer(
+  s: State,
+  input: NewTime
+): { newState: State; output?: NewTempo } {
   let changes: LinkedList<Change> = s.changes;
-  let output: NewTempo|null = null;
+  let output: NewTempo | null = null;
   while (!isEmptyList(changes)) {
-    if (getHead<Change>(changes).time >= input.newTimeMs) {
+    if (getHead<Change>(changes).time <= input.newTimeMs) {
       output = makeTempoEvent(getHead<Change>(changes).tempo);
       changes = getTail(changes);
     } else {
       break;
     }
   }
-  return output ? {newState: {changes}, output} : {newState: {changes}};
+  return output ? { newState: { changes }, output } : { newState: { changes } };
 }
 
 /** Get a tempo-scheduling state machine. */
